b76b5181946dc03350083fcdfa6b67d2c1e947e5
Jelang R. Kharisma <jelangrkharisma@gmail.com>
2020-01-17 14:12:02 +0700

    built using parcel, tinggal mecah2

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <search-bar></search-bar>
+</template>
+
+
+<script>
+import searchBar from "./components/search-bar";
+
+export default {
+  name: "App",
+  components: {
+    searchBar
+  }
+};
+</script>
+
+
+
+
+<style>
+</style>
